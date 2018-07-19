85ff8bc9f4278bec298950b109c4e6714c044cd5
Doug Cole <idugcoal@gmail.com>
2018-07-19 08:45:22 -0700

    updates title

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Preview from './Preview';
 import Help from './Help';
 import NotFound from './NotFound';
 
+import Home from './Home';
 import Page from './Page';
 import Article from './Article';
 
@@ -19,6 +20,7 @@ const App = (props) => (
     <Switch>
       <Redirect exact from="/" to="/help"/>
       <Route exact path="/help" component={Help} />
+      <Route exact path="/home" render={routeProps => <Home {...routeProps} prismicCtx={props.prismicCtx} />} />
       <Route exact path="/preview" render={routeProps => <Preview {...routeProps} prismicCtx={props.prismicCtx} />} />
       <Route exact path="/page/:uid" render={routeProps => <Page {...routeProps} prismicCtx={props.prismicCtx} />} />
       <Route exact path="/article/:uid" render={routeProps => <Article {...routeProps} prismicCtx={props.prismicCtx} />} />      
